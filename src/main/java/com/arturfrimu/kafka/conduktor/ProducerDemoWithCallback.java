6cfca8be191760abb2784a11685895e02058cf61
Artur Frimu <artur.frimu2@gmail.com>
2023-09-27 08:58:36 +0300

    the record was successfully sent

--- src/main/java/com/arturfrimu/kafka/conduktor/ProducerDemoWithCallback.java
+++ src/main/java/com/arturfrimu/kafka/conduktor/ProducerDemoWithCallback.java
@@ -28,6 +28,16 @@ public class ProducerDemoWithCallback {
                 @Override
                 public void onCompletion(RecordMetadata metadata, Exception exception) {
                     // executes every time a record successfully sent or an exception is thrown
+                    if (exception == null) {
+                        // the record was successfully sent
+                        log.info(
+                                "Received new metadata\n" +
+                                "Topic: " + metadata.topic() + "\n",
+                                "Partition: " + metadata.partition() + "\n",
+                                "Offset: " + metadata.offset() + "\n",
+                                "Timestamp: " + metadata.timestamp() + "\n"
+                        );
+                    }
                 }
             });
 
