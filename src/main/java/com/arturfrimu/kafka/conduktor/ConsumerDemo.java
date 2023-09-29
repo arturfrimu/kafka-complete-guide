07d29c4452dae885091bc973765c07888b4128ce
Artur Frimu <artur.frimu2@gmail.com>
2023-09-29 10:12:46 +0300

    pool for data DANGEROYS !!

--- src/main/java/com/arturfrimu/kafka/conduktor/ConsumerDemo.java
+++ src/main/java/com/arturfrimu/kafka/conduktor/ConsumerDemo.java
@@ -1,13 +1,12 @@
 package com.arturfrimu.kafka.conduktor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Properties;
 
@@ -34,5 +33,19 @@ public class ConsumerDemo {
         // subscribe to a topic
         String topic = "demo_topic";
         consumer.subscribe(List.of(topic));
+
+        // pool for data
+        while (true) {
+            log.info("Pooling");
+
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(5000));
+
+            records.forEach(record -> log.info("Key: %s, Value: %s, Partition: %s, Offset: %s".formatted(
+                    record.key(),
+                    record.value(),
+                    record.partition(),
+                    record.offset()))
+            );
+        }
     }
 }
