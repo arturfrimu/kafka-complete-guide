ecc540a725926aa279801ef9b36ae2f858cf8349
Artur Frimu <artur.frimu2@gmail.com>
2023-09-29 09:11:45 +0300

    Create my first consumer in Java Code ConsumerDemo !!!!

---
+++ src/main/java/com/arturfrimu/kafka/conduktor/ConsumerDemo.java
@@ -0,0 +1,22 @@
+package com.arturfrimu.kafka.conduktor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+
+@Slf4j
+@Component
+public class ConsumerDemo {
+    public static void main(String[] args) {
+        log.info("I'm a simple kafka consumer");
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // create consumer config
+    }
+}
