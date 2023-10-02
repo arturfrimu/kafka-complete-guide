e7eb46131d8129c9113742e08c0b5ed0b5a8a1bf
Artur Frimu <artur.frimu2@gmail.com>
2023-10-02 09:11:48 +0300

    create Event Source Builder

--- kafka-producer-wikimedia/src/main/java/com/arturfrimu/kafka/producer/wikimedia/producer/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/com/arturfrimu/kafka/producer/wikimedia/producer/WikimediaChangesProducer.java
@@ -1,9 +1,11 @@
 package com.arturfrimu.kafka.producer.wikimedia.producer;
 
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.beans.EventHandler;
+import java.net.URI;
 import java.util.Properties;
 
 public class WikimediaChangesProducer {
@@ -19,5 +21,10 @@ public class WikimediaChangesProducer {
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
         String topic = "wikimedia.recentchange";
+
+        EventHandler eventHandler = null; // TODO: 02.10.2023
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
     }
 }
