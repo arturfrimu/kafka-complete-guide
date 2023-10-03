6423215d792f809b83a408101c0b828e836b55ec
Artur Frimu <artur.frimu2@gmail.com>
2023-10-03 15:13:41 +0300

    add WikimediaChangeHandler implements EventHandler

---
+++ kafka-producer-wikimedia/src/main/java/com/arturfrimu/kafka/producer/wikimedia/handler/WikimediaChangeHandler.java
@@ -0,0 +1,32 @@
+package com.arturfrimu.kafka.producer.wikimedia.handler;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.MessageEvent;
+
+public class WikimediaChangeHandler implements EventHandler {
+
+    @Override
+    public void onOpen() throws Exception {
+
+    }
+
+    @Override
+    public void onClosed() throws Exception {
+
+    }
+
+    @Override
+    public void onMessage(String event, MessageEvent messageEvent) throws Exception {
+
+    }
+
+    @Override
+    public void onComment(String comment) throws Exception {
+
+    }
+
+    @Override
+    public void onError(Throwable t) {
+
+    }
+}
