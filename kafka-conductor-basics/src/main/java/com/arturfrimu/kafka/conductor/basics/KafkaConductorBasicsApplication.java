6c87015919cd392c5c30b6ee09c67c6c0557d485
Artur Frimu <artur.frimu2@gmail.com>
2023-10-03 15:14:06 +0300

    add a new module for basic lessons

---
+++ kafka-conductor-basics/src/main/java/com/arturfrimu/kafka/conductor/basics/KafkaConductorBasicsApplication.java
@@ -0,0 +1,11 @@
+package com.arturfrimu.kafka.conductor.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConductorBasicsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConductorBasicsApplication.class, args);
+    }
+}
